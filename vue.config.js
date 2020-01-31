bee1278e2ecd08ee81326523f296b21894f08448
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2020-02-01 00:42:09 +0700

    Move frontend code to api folder

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+    pages: {
+        index: {
+            entry: 'api/src/main.js',
+            template: 'api/public/index.html'
+        }
+    },
     devServer: {
         proxy: 'http://localhost:8000'
     }
